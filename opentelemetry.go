ba31f71ce39c2c6be8517ec002e54b3bb272cd47
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2024-09-18 23:10:05 +0700

    update go mod

--- opentelemetry.go
+++ opentelemetry.go
@@ -34,10 +34,6 @@ func recordError(span trace.Span, err error) {
 }
 
 func (t *Otel) TracePublisherStart(ctx context.Context, payload *basicPublish) context.Context {
-	if !trace.SpanFromContext(ctx).IsRecording() {
-		return ctx
-	}
-
 	spanName := "Rabbitmq Publish Message"
 	ctx, span := t.tracer.Start(ctx, spanName)
 	span.SetAttributes(
@@ -61,6 +57,7 @@ func (t *Otel) TracePublisherStart(ctx context.Context, payload *basicPublish) c
 		attribute.String("messaging.rabbitmq.properties.app_id", payload.Properties.AppId),
 		attribute.String("messaging.rabbitmq.properties.headers", formatHeaders(payload.Properties.Headers)),
 	)
+	span.End()
 	return ctx
 }
 
